3fc1e7212c333146907508754d79cb6a9c508544
Volkan Kumbasar <7557299+kumbasar@users.noreply.github.com>
2020-12-31 01:06:09 +0300

    Add Scale and convert images using PIL example

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ IT Automation with Python
 ## Examples
 - Log CSV reporter
 - Daily Sync 
-- Employees' start date report 
+- Employees' start date report
+- Scale and convert images
---
+++ ScaleAndConvertImages/image_convert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+
+def rotateResizeConvert(path, output, x=128, y=128, rotate=-90):
+
+    for (dirpath, dirnames, filenames) in os.walk(path):
+
+        for image in filenames:
+
+            # Skip hidden files
+            if '.' == image[0]:
+                continue
+
+            # Load TIFF image
+            im = Image.open(os.path.join(dirpath, image))
+            new_im = im.rotate(rotate).resize((x, y)).convert("RGB")
+            new_image_file = os.path.join(output, image)
+            print("{} => {}".format(image, new_image_file))
+
+            # Load Save JPEG image
+            new_im.save(new_image_file, "JPEG")
+
+
+if __name__ == "__main__":
+    source = 'images'
+    output = '/opt/icons'
+    rotateResizeConvert('images', '/opt/icons')
